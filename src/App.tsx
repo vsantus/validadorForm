0073dc07b35e6b141117b043e91f316815fbf1ef
devsantos <viniciusantosd.abreu@gmail.com>
2025-07-30 20:03:17 -0300

    feat: create user registration form with validation using React Hook Form and Zod

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Login from "./components/Login"
+import RegisterPage from "./components/Register"
+import UserProfile from "./components/UserProfile"
 
 function App() {
-  
+
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter> <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/register" element={<RegisterPage/>} />
+      <Route path="/profile" element={<UserProfile />} />
+    </Routes>
+    </BrowserRouter>
   )
 }
 
